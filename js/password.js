3091501f18d83f906d275975d854c29547767997
mahookoko003-create <mahookoko003@gmail.com>
2026-05-20 20:54:21 +0200

    Add password generation function

--- js/password.js
+++ js/password.js
@@ -1,1 +1,18 @@
+function generatePassword(){
 
+  let chars =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  let password = "";
+
+  for(let i = 0; i < 12; i++){
+
+    password += chars.charAt(
+      Math.floor(Math.random() * chars.length)
+    );
+
+  }
+
+  document.getElementById("password").innerText = password;
+
+}
